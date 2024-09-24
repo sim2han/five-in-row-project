5481af778baccca261e3a7203728d07563706e5c
sim2han <hssimsim153@gmail.com>
2024-09-24 20:19:55 +0900

    init project

---
+++ fir_game/src/lib.rs
@@ -0,0 +1,89 @@
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+enum FirGameError {
+    #[error("invalid index access in baord")]
+    InvalidIndexAccessInBoard,
+}
+
+#[derive(Copy, Clone, Debug)]
+struct FirBoardSize {
+    pub x : usize,
+    pub y : usize,
+}
+
+impl FirBoardSize {
+    pub fn sqaure(n : usize) -> Self {
+        FirBoardSize {
+            x : n,
+            y : n,
+        }
+    }
+
+    pub fn rectangle(x : usize, y : usize) -> Self {
+        FirBoardSize {
+            x, y
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+enum SqaureState {
+    Empty,
+    Black,
+    White
+}
+
+#[derive(Copy, Clone, Debug)]
+enum Order {
+    Black,
+    White,
+}
+
+#[derive(Clone, Debug)]
+struct FirGameState {
+    size : FirBoardSize,
+    board : Vec<SqaureState>,
+}
+
+impl FirGameState {
+    pub fn empty_board(size : FirBoardSize) -> Self {
+        FirGameState {
+            size,
+            board : vec![SqaureState::Empty; size.x * size.y],
+        }
+    }
+
+    pub fn get_size(&self) -> FirBoardSize {
+        self.size
+    }
+
+    pub fn get_square(&self, x : i32, y : i32) -> Result<SqaureState, FirGameError> { // -> Result<>
+        if 0 <= x && x < self.size.x && 0 <= y && y <= self.size.y {
+            self.board[y * self.size.x + x]
+        }
+        else {
+            FirGameError::InvalidIndexAccessInBoard
+        }
+    }
+}
+
+/// Game Player of five in row.
+struct FirGame {
+
+}
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
---
+++ fir_server/src/main.rs
@@ -0,0 +1,7 @@
+use fir_game::add;
+
+fn main() {
+    println!("Hello, world!");
+
+    add(1, 2);
+}
